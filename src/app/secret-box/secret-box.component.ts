22e4f61a526e3ed7a02deb02482ea7ce8005a312
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-01-10 23:52:47 +0100

    (component:secret-box) use @Input-Bindings

--- src/app/secret-box/secret-box.component.ts
+++ src/app/secret-box/secret-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secret-box',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./secret-box.component.css']
 })
 export class SecretBoxComponent implements OnInit {
-  isHidingSecret = true;
+  @Input() isHidingSecret = true;
+  @Input() secret: string;
 
   constructor() { }
 
